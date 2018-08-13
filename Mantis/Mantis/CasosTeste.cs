284b1bb237b0f627550e65c572c3be9f0496b2de
Patrick Reis <reis.sorrentino@gmail.com>
2018-08-13 16:41:48 -0300

    Caso de testes realiza Login

--- Mantis/Mantis/CasosTeste.cs
+++ Mantis/Mantis/CasosTeste.cs
@@ -1,14 +1,45 @@
-using System;
 using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using Mantis.PageObjects;
+using System;
 
 namespace Mantis
 {
     public class CasosTeste
     {
-        [Fact]
-        public void Test1()
-        {
+        private IWebDriver driver;
+        private LoginPage logar;
 
+        [Theory]
+        [InlineData("patrick.sorrentino", "errada", "Your account may be disabled or blocked or the username/password you entered is incorrect.", "font[color='red']")]
+        [InlineData("errado", "senha123", "Your account may be disabled or blocked or the username/password you entered is incorrect.", "font[color='red']")]
+        [InlineData("patrick.sorrentino", "senha123", "Logged in as: patrick.sorrentino (Patrick Sorrentino - manager)", "[class='login-info-left']")]
+        public void RealizaLogin(string usuario, string senha, string msg, string elemento)
+        {
+            AbreNagevador();
+            logar = new LoginPage(driver);
+            logar.logaMantis(usuario, senha);
+            Thread.Sleep(2000);
+            logar.ValidaMsg(elemento, msg);
+            Fechar();
         }
+
+        public void AbreNagevador()
+        {
+            driver = new ChromeDriver();//@"C:\Users\" + Environment.UserName + @"\Documents\Visual Studio 2017\");
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+            driver.Navigate().GoToUrl("http://mantis-prova.base2.com.br/login_page.php");
+            driver.Manage().Window.Maximize();
+        }
+        public void Fechar()
+        {
+            driver.Close();
+            driver.Quit();
+        }
+
+        
     }
 }
---
+++ Mantis/Mantis/PageObjects/LoginPage.cs
@@ -0,0 +1,53 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using Xunit;
+
+namespace Mantis.PageObjects
+{
+    class LoginPage
+    {
+        private IWebDriver driver;
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = "[name='username']")]
+        public IWebElement user; 
+
+        [FindsBy(How = How.CssSelector, Using = "[name='password']")]
+        public IWebElement password;
+
+        [FindsBy(How = How.CssSelector, Using = "[value='Login']")]
+        public IWebElement entrar;
+
+        [FindsBy(How = How.CssSelector, Using = "[class='login-info-left']")]
+        public IWebElement msgInicial;
+
+        public void logaMantis(string usuario, string senha)
+        {
+            user.SendKeys(usuario);
+            password.SendKeys(senha);
+            entrar.Click();
+
+        }
+
+        public void ValidaMsg(string elemento, string msg)
+        {
+
+            IWebElement msgTela = driver.FindElement(By.CssSelector(elemento));
+            string valor = msgTela.Text;
+            try
+            {
+                bool mnsg = (valor + "").Contains(msg);
+                Assert.True(mnsg);
+            }
+            catch (Exception ex)
+            {
+                Assert.True(false, ex.Message);
+            }
+        }
+    }
+}
